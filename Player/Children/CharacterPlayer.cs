b4b045fc14e76297ccf34372ae0e1ec0c2287a30
Breakcrumps <sonickita7@gmail.com>
2025-06-14 20:07:38 +0300

    Good thing I saved

--- Player/Children/CharacterPlayer.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-public partial class CharacterPlayer : AnimationPlayer
-{
-  public override void _Ready()
-  {
-    AnimationBus.AnimPlay += (animName, blendTime) => { Play(animName, blendTime); };
-  }
-}
--- Player/Composites/Animator.cs
+++ Player/Composites/Animator.cs
@@ -6,13 +6,8 @@ public enum Anim { Idle, Walk, Run, Rise, Fall }
 public partial class Animator : Node
 {
   [ExportGroup("Parameters")]
-  [Export] private float _walkBlendSpeed = 15f;
-  [Export] private float _runBlendSpeed = 15f;
-  [Export] private float _riseBlendSpeed = 7f;
-  [Export] private float _fallBlendSpeed = 7f;
-
-  [Export] private float _shortBlendTime = .25f;
-  [Export] private float _longBlendTime = 1.5f;
+  [Export] private float _fastBlendSpeed = 15f;
+  [Export] private float _slowBlendSpeed = 7f;
 
   public Anim CurrentAnim { private get; set; } = Anim.Idle;
 
@@ -25,55 +20,50 @@ public partial class Animator : Node
   {
     SetBlendValues(delta);
 
-    // UpdateTree();
+    UpdateTree();
   }
 
-  // private void UpdateTree()
-  // {
-  //   EventBus.ChangeBlendParameter("parameters/Walk/blend_amount", _walkValue);
-  //   EventBus.ChangeBlendParameter("parameters/Run/blend_amount", _runValue);
-  //   EventBus.ChangeBlendParameter("parameters/Rise/blend_amount", _riseValue);
-  //   EventBus.ChangeBlendParameter("parameters/Fall/blend_amount", _fallValue);
-  // }
+  private void UpdateTree()
+  {
+    EventBus.ChangeBlendParameter("parameters/Walk/blend_amount", _walkValue);
+    EventBus.ChangeBlendParameter("parameters/Run/blend_amount", _runValue);
+    EventBus.ChangeBlendParameter("parameters/Rise/blend_amount", _riseValue);
+    EventBus.ChangeBlendParameter("parameters/Fall/blend_amount", _fallValue);
+  }
 
   private void SetBlendValues(double delta)
   {
     switch (CurrentAnim)
     {
       case Anim.Idle:
-        // _walkValue = Mathf.Lerp(_walkValue, 0f, _walkBlendSpeed * (float)delta);
-        // _runValue = Mathf.Lerp(_runValue, 0f, _walkBlendSpeed * (float)delta);
-        // _riseValue = Mathf.Lerp(_riseValue, 0f, _riseBlendSpeed * (float)delta);
-        // _fallValue = Mathf.Lerp(_fallValue, 0f, _fallBlendSpeed * (float)delta);
-        AnimationBus.PlayAnim("Idle", _shortBlendTime);
+        _walkValue = Mathf.Lerp(_walkValue, 0f, _fastBlendSpeed * (float)delta);
+        _runValue = Mathf.Lerp(_runValue, 0f, _fastBlendSpeed * (float)delta);
+        _riseValue = Mathf.Lerp(_riseValue, 0f, _fastBlendSpeed * (float)delta);
+        _fallValue = Mathf.Lerp(_fallValue, 0f, _slowBlendSpeed * (float)delta);
         break;
       case Anim.Walk:
-        // _walkValue = Mathf.Lerp(_walkValue, 1f, _walkBlendSpeed * (float)delta);
-        // _runValue = Mathf.Lerp(_runValue, 0f, _walkBlendSpeed * (float)delta);
-        // _riseValue = Mathf.Lerp(_riseValue, 0f, _riseBlendSpeed * (float)delta);
-        // _fallValue = Mathf.Lerp(_fallValue, 0f, _walkBlendSpeed * (float)delta);
-        AnimationBus.PlayAnim("Walk", _shortBlendTime);
+        _walkValue = Mathf.Lerp(_walkValue, 1f, _fastBlendSpeed * (float)delta);
+        _runValue = Mathf.Lerp(_runValue, 0f, _fastBlendSpeed * (float)delta);
+        _riseValue = Mathf.Lerp(_riseValue, 0f, _fastBlendSpeed * (float)delta);
+        _fallValue = Mathf.Lerp(_fallValue, 0f, _slowBlendSpeed * (float)delta);
         break;
       case Anim.Run:
-        // _walkValue = Mathf.Lerp(_walkValue, 0f, _walkBlendSpeed * (float)delta);
-        // _runValue = Mathf.Lerp(_runValue, 1f, _walkBlendSpeed * (float)delta);
-        // _riseValue = Mathf.Lerp(_riseValue, 0f, _riseBlendSpeed * (float)delta);
-        // _fallValue = Mathf.Lerp(_fallValue, 0f, _walkBlendSpeed * (float)delta);
-        AnimationBus.PlayAnim("Run", _shortBlendTime);
+        _walkValue = Mathf.Lerp(_walkValue, 0f, _fastBlendSpeed * (float)delta);
+        _runValue = Mathf.Lerp(_runValue, 1f, _fastBlendSpeed * (float)delta);
+        _riseValue = Mathf.Lerp(_riseValue, 0f, _fastBlendSpeed * (float)delta);
+        _fallValue = Mathf.Lerp(_fallValue, 0f, _slowBlendSpeed * (float)delta);
         break;
       case Anim.Rise:
-        // _walkValue = Mathf.Lerp(_walkValue, 0f, _walkBlendSpeed * (float)delta);
-        // _runValue = Mathf.Lerp(_runValue, 0f, _walkBlendSpeed * (float)delta);
-        // _riseValue = Mathf.Lerp(_riseValue, 1f, _riseBlendSpeed * (float)delta);
-        // _fallValue = Mathf.Lerp(_fallValue, 0f, _fallBlendSpeed * (float)delta);
-        AnimationBus.PlayAnim("Rise", _shortBlendTime);
+        _walkValue = Mathf.Lerp(_walkValue, 0f, _fastBlendSpeed * (float)delta);
+        _runValue = Mathf.Lerp(_runValue, 0f, _fastBlendSpeed * (float)delta);
+        _riseValue = Mathf.Lerp(_riseValue, 1f, _fastBlendSpeed * (float)delta);
+        _fallValue = Mathf.Lerp(_fallValue, 0f, _slowBlendSpeed * (float)delta);
         break;
       case Anim.Fall:
-        _walkValue = Mathf.Lerp(_walkValue, 0f, _walkBlendSpeed * (float)delta);
-        _runValue = Mathf.Lerp(_runValue, 0f, _walkBlendSpeed * (float)delta);
-        _riseValue = Mathf.Lerp(_riseValue, 0f, _riseBlendSpeed * (float)delta);
-        _fallValue = Mathf.Lerp(_fallValue, 1f, _fallBlendSpeed * (float)delta);
-        AnimationBus.PlayAnim("Fall", _longBlendTime);
+        _walkValue = Mathf.Lerp(_walkValue, 0f, _fastBlendSpeed * (float)delta);
+        _runValue = Mathf.Lerp(_runValue, 0f, _fastBlendSpeed * (float)delta);
+        _riseValue = Mathf.Lerp(_riseValue, 0f, _fastBlendSpeed * (float)delta);
+        _fallValue = Mathf.Lerp(_fallValue, 1f, _slowBlendSpeed * (float)delta);
         break;
     }
   }
